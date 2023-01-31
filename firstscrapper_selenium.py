7029f515342a9db2369747bbaafaf6a1ebb91a4f
abc12345d <zhiqi@Zhis-MacBook-Pro.local>
2023-01-31 14:04:55 +0000

    code for my first Selenium scrapper

--- firstscrapper_selenium.py
+++ firstscrapper_selenium.py
@@ -8,3 +8,53 @@
 # (4) select country within dropdown as "Spain" 
 #       - pause the execution of code for 3 seconds to prevent Selenium's "TimeoutException" during
 
+#%%
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+import time
+
+webpage = "https://www.adamchoi.co.uk/overs/detailed"
+path_webdriver = '../webdriver_for_webscraping/chromedriver_mac_arm64/chromedriver'
+# initialize a Chrome webdriver
+driver = webdriver.Chrome(path_webdriver)
+# Load the webpage in a new browser window.
+driver.get(webpage)
+
+# locate "all matches" button
+all_matches_button = driver.find_element_by_xpath('//label[@analytics-event="All matches"]')
+all_matches_button.click()
+
+#%%
+# locate "country" dropbox
+country_dropbox = Select(driver.find_element_by_xpath('//select[@id="country"]'))
+country_dropbox.select_by_visible_text("Spain")
+
+# pause the execution of code for 3 seconds
+time.sleep(3)
+
+# scrap table 
+date_list = []
+home_list = []
+score_list = []
+away_list = []
+for row in driver.find_elements_by_tag_name('tr'):
+    date = row.find_element_by_xpath('./td[1]').text
+    date_list.append(date)
+    home = row.find_element_by_xpath('./td[2]').text
+    home_list.append(home)
+    score = row.find_element_by_xpath('./td[3]').text
+    score_list.append(score)
+    away = row.find_element_by_xpath('./td[4]').text
+    away_list.append(away)
+    
+#%%
+# store table contents as a pandas dataframe
+df = pd.DataFrame(data= {"date":date_list, "home_team":home_list, "score":score_list, "away_team":away_list})
+df.to_csv("football_data.csv", index=False)
+
+# %%
+# turn off the Chrome webdriver (an important step!!)
+driver.quit()
+
+# %%
